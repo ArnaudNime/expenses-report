a9a36b04851c0709c1e7fd5d32cf2dda3fd58fef
Thomas Deletraz <thomas.deletraz@sogexia.com>
2023-08-01 23:51:41 +0200

    Install Api plateform

---
+++ migrations/database_creation.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS `user`
+(
+    id       int,
+    lastname varchar(80),
+    name     varchar(80),
+    birtdate date,
+    email    varchar(80)
+);
+
+
+CREATE TABLE IF NOT EXISTS `company`
+(
+    id       int,
+    lastname varchar(80),
+    name     varchar(80),
+    birtdate date,
+    email    varchar(80)
+);
