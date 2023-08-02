e7b6a7493070d908c673a59f0065ee71b8bf91fc
Thomas Deletraz <thomas.deletraz@sogexia.com>
2023-08-03 00:51:31 +0200

    Update fixtures

--- src/Entity/Commercial.php
+++ src/Entity/Commercial.php
@@ -22,11 +22,51 @@ final class Commercial
     #[ORM\Id, ORM\Column, ORM\GeneratedValue]
     private int $id;
 
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    private string $name;
 
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    private string $lastname;
+
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    private DateTime $birthdate;
+
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    private string $email;
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    private DateTime $creationDate;
     public function getId(): int
     {
         return $this->id;
     }
 
+    public function getName(): string
+    {
+        return $this->name;
+    }
 
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    public function getBirthdate(): DateTime
+    {
+        return $this->birthdate;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getCreationDate(): DateTime
+    {
+        return $this->creationDate;
+    }
 }
--- test/fixtures/user_1.sql
+++ test/fixtures/user_1.sql
@@ -1,5 +1,5 @@
 /* user */
-INSERT INTO "user" (id, lastname, name, birthdate, email, creation_date)
+INSERT INTO commercial (id, lastname, name, birthdate, email, creation_date)
 VALUES (1, 'Jean', 'Tam', '2002-05-17', 'jean.tam@ledev.eu', '2023-08-02 20:30:10.000000')
 ON CONFLICT DO NOTHING;
 /* company */
@@ -16,28 +16,28 @@ INSERT INTO company (id, name, email, creation_date)
 VALUES (4, 'carrefour', 'nocibe@carrefour.com', '2023-08-02 20:30:10.000000')
 ON CONFLICT DO NOTHING;
 /* expense_report */
-INSERT INTO expense_report (id, amount, type, company, "user", payment_date, creation_date)
+INSERT INTO expense_report (id, amount, type, company, commercial, payment_date, creation_date)
 VALUES (1, 100, 'gazoline', 1, 1, '2023-08-02', '2023-08-02 20:30:10.000000')
 ON CONFLICT DO NOTHING;
-INSERT INTO expense_report (id, amount, type, company, "user", payment_date, creation_date)
+INSERT INTO expense_report (id, amount, type, company, commercial, payment_date, creation_date)
 VALUES (2, 15.50, 'meat', 1, 1,'2023-08-02', '2023-08-02 20:30:10.000000')
 ON CONFLICT DO NOTHING;
-INSERT INTO expense_report (id, amount, type, company, "user", payment_date, creation_date)
+INSERT INTO expense_report (id, amount, type, company, commercial, payment_date, creation_date)
 VALUES (3, 12.50, 'toll', 1, 1,'2023-08-02', '2023-08-02 20:30:10.000000')
 ON CONFLICT DO NOTHING;
-INSERT INTO expense_report (id, amount, type, company, "user", payment_date, creation_date)
+INSERT INTO expense_report (id, amount, type, company, commercial, payment_date, creation_date)
 VALUES (4, 1000, 'conference', 1, 1,'2023-08-02', '2023-08-02 20:30:10.000000')
 ON CONFLICT DO NOTHING;
 
-INSERT INTO expense_report (id, amount, type, company, "user", payment_date, creation_date)
+INSERT INTO expense_report (id, amount, type, company, commercial, payment_date, creation_date)
 VALUES (5, 20, 'gazoline', 2, 1,'2023-08-02', '2023-08-02 20:30:10.000000')
 ON CONFLICT DO NOTHING;
-INSERT INTO expense_report (id, amount, type, company, "user", payment_date, creation_date)
+INSERT INTO expense_report (id, amount, type, company, commercial, payment_date, creation_date)
 VALUES (6, 22.50, 'meat', 2, 1,'2023-08-02', '2023-08-02 20:30:10.000000')
 ON CONFLICT DO NOTHING;
-INSERT INTO expense_report (id, amount, type, company, "user", payment_date, creation_date)
+INSERT INTO expense_report (id, amount, type, company, commercial, payment_date, creation_date)
 VALUES (7, 1.50, 'toll', 2, 1,'2023-08-02', '2023-08-02 20:30:10.000000')
 ON CONFLICT DO NOTHING;
-INSERT INTO expense_report (id, amount, type, company, "user", payment_date, creation_date)
+INSERT INTO expense_report (id, amount, type, company, commercial, payment_date, creation_date)
 VALUES (8, 1000, 'conference', 2, 1,'2023-08-02', '2023-08-02 20:30:10.000000')
 ON CONFLICT DO NOTHING;
