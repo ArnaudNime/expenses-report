066b4ef763d59b9444f016d74d625c885b9a728f
Thomas Deletraz <thomas.deletraz@sogexia.com>
2023-08-14 21:08:06 +0200

    Complete Makefile and docker conf

--- Makefile
+++ Makefile
@@ -4,5 +4,11 @@ build:
 start:
 	docker-compose up -d
 	symfony server:start --no-tls
+stop:
+	docker-compose down
+	symfony server:stop
+test:
+	./vendor/bin/phpunit --testsuite unit
+	./vendor/bin/phpunit --testsuite http
 fixtures:
-	docker exec -i postgres psql app -Udev_user < test/fixtures/user_1.sql
+	docker exec -i postgres psql app -Udev_user < tests/fixtures/user_1.sql
--- docker/database/base.sql
+++ docker/database/base.sql
@@ -1,9 +1,17 @@
 CREATE TABLE IF NOT EXISTS commercial
 (
     id            serial primary key,
+    firstname     varchar(80)        not null,
     lastname      varchar(80)        not null,
-    name          varchar(80)        not null,
     birthdate     date               not null,
+    email         varchar(80) unique not null,
+    creation_date timestamp          not null
+);
+
+CREATE TABLE IF NOT EXISTS company
+(
+    id            serial primary key,
+    name          varchar(80) unique not null,
     email         varchar(80) unique not null,
     creation_date timestamp          not null
 );
@@ -14,18 +22,18 @@ CREATE TABLE IF NOT EXISTS expense_report
     amount        float       not null,
     type          varchar(80) not null,
     payment_date  date        not null,
-    company       int         not null,
-    commercial        int         not null,
+    company_id    int         not null,
+    commercial_id int         not null,
     creation_date timestamp   not null
 );
 
-CREATE INDEX expense_report_company_idx ON expense_report (company);
-CREATE INDEX expense_report_user_idx ON "expense_report" ("commercial");
+ALTER TABLE expense_report
+    ADD CONSTRAINT company_fk
+        FOREIGN KEY (company_id) REFERENCES company(id);
 
-CREATE TABLE IF NOT EXISTS company
-(
-    id            serial primary key,
-    name          varchar(80) not null,
-    email         varchar(80) not null,
-    creation_date timestamp   not null
-);
+ALTER TABLE expense_report
+    ADD CONSTRAINT commercial_fk
+        FOREIGN KEY (commercial_id) REFERENCES commercial(id);
+
+CREATE INDEX expense_report_company_idx ON expense_report (company_id);
+CREATE INDEX expense_report_user_idx ON "expense_report" ("company_id");
