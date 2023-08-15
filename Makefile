e0bf5796119d34787812a3a4c3e50bef3041b1fc
Thomas Deletraz <thomas.deletraz@sogexia.com>
2023-08-15 17:15:49 +0200

    complete docker configuration

--- Makefile
+++ Makefile
@@ -1,14 +1,15 @@
 build:
 	docker-compose rm -f
 	docker-compose build --no-cache
+	composer install
 start:
 	docker-compose up -d
-	symfony server:start --no-tls
 stop:
 	docker-compose down
 	symfony server:stop
 test:
-	./vendor/bin/phpunit --testsuite unit
-	./vendor/bin/phpunit --testsuite http
+	docker-compose exec -it php /var/www/symfony_docker/vendor/bin/phpunit --testsuite unit
+	docker-compose exec -it php /var/www/symfony_docker/vendor/bin/phpunit --testsuite http
 fixtures:
 	docker exec -i postgres psql app -Udev_user < tests/fixtures/user_1.sql
+clear-cache:
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,7 +13,7 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
 if (!function_exists('getPdoForTest')) {
     function getPdoForTest(): PDO
     {
-        $pdo = new PDO('pgsql:host=127.0.0.1;port=5433;dbname=app;user=dev_user;password=dev_password');
+        $pdo = new PDO('pgsql:host=172.25.0.11;port=5432;dbname=app;user=dev_user;password=dev_password');
         $pdo->exec('TRUNCATE expense_report, company, commercial CASCADE');
         $pdo->exec('ALTER SEQUENCE company_id_seq RESTART WITH 1');
         $pdo->exec('ALTER SEQUENCE commercial_id_seq RESTART WITH 1');
